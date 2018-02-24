d67a6ebe5b16d594b9f50c291ae6d25a2f85dc7c
Megatron\PhazedOut <swanninger@gmail.com>
2018-02-24 12:52:52 -0600

    Init

---
+++ src/main/java/com/xds/XdsApplication.java
@@ -0,0 +1,12 @@
+package com.xds;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class XdsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(XdsApplication.class, args);
+	}
+}
